c7c1435eb410291d82609f65472ff4eedb7ddad2
nguyenthanhan201 <fxannguyen201@gmail.com>
2023-10-05 17:08:47 +0700

    feat: update package to version 1.6.0

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,10 +1,10 @@
-import type { StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 import { Button } from './Button';
 import DocumentationTemplate from './mdx/Configure.mdx';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-const meta = {
+const meta: Meta = {
   title: 'Example/Button',
   component: Button,
   parameters: {
